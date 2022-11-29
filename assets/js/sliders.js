c6e2df22f74ea3e450e34820d95cafcf553013dc
Oleksandr Harahan <aleksandrharahan@Aleksandrs-MacBook-Air.local>
2022-11-29 10:17:53 +0200

    [FRONTPAGE][PRICING] Display bug with plans
    https://freedcamp.com/view/3046321/tasks/49590176

--- assets/js/sliders.js
+++ assets/js/sliders.js
@@ -1,38 +1,86 @@
-$('.pricing_slider').slick({
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    prevArrow: $('.prev'),
-    nextArrow: $('.next'),
-    dots: false,
-    centerMode: true,
-    // focusOnSelect: true,
-    // variableWidth: true,
-    responsive: [
-        {
-            breakpoint: 1025,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 2,
-                infinite: true,
+// $('.pricing_slider').slick({
+//     speed: 300,
+//     slidesToShow: 4,
+//     slidesToScroll: 1,
+//     prevArrow: $('.prev'),
+//     nextArrow: $('.next'),
+//     dots: false,
+//     responsive: [
+//         {
+//             breakpoint: 1025,
+//             settings: {
+//                 slidesToShow: 3,
+//                 slidesToScroll: 1,
+//                 infinite: true,
+//                 centerMode: true,
+//             }
+//         },
+//         {
+//             breakpoint: 769,
+//             settings: {
+//                 slidesToShow: 2,
+//                 slidesToScroll: 1,
+//                 centerMode: true,
+//             }
+//         },
+//         {
+//             breakpoint: 540,
+//             settings: {
+//                 slidesToShow: 1,
+//                 slidesToScroll: 1,
+//                 centerMode: true,
+//             }
+//         }
+//     ]
+// });
+
+
+
+$(".pricing_slider").on("destroy", function(_, slick) {
+    const reinit = $.debounce(200, () => {
+            console.log('debounce')
+            if (slick.activeBreakpoint < window.innerWidth) return;
+            $(this).slick(slick.options);
+            window.removeEventListener("resize", reinit);
+        });
+        window.addEventListener("resize", reinit);
+    })
+    .slick({
+        slidesToShow: 1,
+        arrows: false,
+        dots: false,
+        mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 1025,
+                settings: "unslick"
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 540,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                }
+            },
+            {
+                breakpoint: 320,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    centerMode: true,
+                }
             }
-        },
-        {
-            breakpoint: 769,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 540,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-    ]
-});
+        ]
+    });
 
 $('.blocks_slider-management').slick({
     speed: 300,
